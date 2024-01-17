d487d4673f0f3dba168290a45f61d5fecf0e1cef
sarahgarba <106061797+sarahgarba@users.noreply.github.com>
2024-01-17 17:25:19 +0100

    second

--- src/app/dashboard/diagnostic/page.tsx
+++ src/app/dashboard/diagnostic/page.tsx
@@ -5,12 +5,14 @@ import {
 } from "@/components"
 
 import React, { useState } from 'react';
+import { ChangeEvent } from 'react';
 
 
 export default function DiagnosticRecent() {
-    function handleSearchChange(event: ChangeEvent<HTMLInputElement>): void {
-        throw new Error("Function not implemented.");
-    }
+    
+    function handleSearchChange(event: any): void {
+        // Handle the change event
+      }
 
     return (
         <div className="w-full h-full overflow-y-auto flex flex-col  rounded-xl border gap-3">
